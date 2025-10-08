d7d6d533cc1b150a1995eebe1896c4417da4fe32
prajwalpatil18 <92904433+prajwalpatil18@users.noreply.github.com>
2025-10-08 14:17:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ from langchain_groq import ChatGroq
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import TextLoader
 from langchain.chains import create_history_aware_retriever, create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 
@@ -208,9 +209,8 @@ else:
     # ------------------------
     #uploaded_files = st.sidebar.file_uploader("Upload PDF(s)", type="pdf", accept_multiple_files=True)
     #documents = []
-    path = f"./data.pdf"
-    documents = []
-    loader = PyPDFLoader(path)
+    path = f"./data.txt"
+    loader = TextLoader(path)
     docs = loader.load()
     #documents.extend(docs)
     print(f"Documents loaded: {len(docs)}")
